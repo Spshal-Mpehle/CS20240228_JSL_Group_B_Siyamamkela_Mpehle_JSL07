cda9a420876d6abb07ccd8f3fa8c3f707ef45011
Siyamamkela <mpehlesiyamamkela@gmail.com>
2025-02-26 06:47:14 +0200

    progressing with the project.

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ document.addEventListener('DOMContentLoaded', function () {
   
       // 🚨 Get input values
       const studentNameInput = document.getElementById('studentName');
-      const personalMessageInput = ;
-      const courseNameInput = ; 
+      const personalMessageInput = document.getElementById('personalMessage');
+      const courseNameInput = document.getElementById('courseName'); 
   
       const studentName = studentNameInput.value;
       const personalMessage = personalMessageInput.value;
@@ -25,8 +25,10 @@ document.addEventListener('DOMContentLoaded', function () {
       }
   
       // 🚨 Generate certificate content dynamically
-      certificateContent. = `
+      certificateContent.innerHTML = `
       <h3>${studentName}</h3>
+      <p>${personalMessage}</p>
+      <p>Course: ${courseName}</p>
     `;
     
       //  Display the modal
@@ -39,8 +41,8 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   
     //  🚨 Close the modal when the close button is clicked
-    closeModal.('', function () {
-      
-    });
+    closeModal.addEventListener('click', function () => {
+      modal.style.display = 'none',
+      });
   });
   
